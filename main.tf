5d9879a79d28ad3f169f311309e0ea001c5eee60
Alexunder98 <alexcristian1008@gmail.com>
2026-03-02 15:58:47 +0200

    Added Load Balancer

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ module "blog_alb" {
       port     = 80
       protocol = "HTTP"
       forward = {
-        target_group_arn = aws.lb_target_group.blog.arn
+        target_group_arn = aws_lb_target_group.blog.arn
       }
     }
   }
